72e7fc59524c2fa362a71ed554d2bd96c9ec3101
Zhuyi Xue (SciNet) <zhuyi.xue@mail.utoronto.ca>
2013-06-07 13:24:36 -0400

    big update

--- oplsaa/lala.py
+++
@@ -1,9 +0,0 @@
-import glob
-import os
-
-fs = glob.glob("sq2[1-8]_*[0-9].itp")
-
-for f in fs:
-    sf = f.split("_")
-    os.rename(f, sf[0] + sf[1][-4:])
-    # print f, "{0}_{1}".format(sf[0], sf[2]))
